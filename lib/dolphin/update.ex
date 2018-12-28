b451d700710a4b0e58d05c831d50ff44d1f51a72
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-12-28 11:29:12 +0100

    Update.filename/1 strips mentions from the update

--- lib/dolphin/update.ex
+++ lib/dolphin/update.ex
@@ -10,11 +10,14 @@ defmodule Dolphin.Update do
 
       iex> Dolphin.Update.filename(%Dolphin.Update{text: "$ man ed\n\n#currentstatus"})
       "2018-12-27-man-ed-currentstatus.md"
+      iex> Dolphin.Update.filename(%Dolphin.Update{text: "@judofyr@ruby.social because ed is the standard text editor (https://www.gnu.org/fun/jokes/ed-msg.txt)!"})
+      "2018-12-27-because-ed-is-the-standard.md"
 
   """
   def filename(%Dolphin.Update{text: text}) do
     (@date.to_iso8601(@date.utc_today) <> "-" <> text)
-    |> Dolphin.Utils.filter_characters([' ', '\n', '-', ?a..?z, ?A..?Z, ?0..?9])
+    |> Dolphin.Utils.filter_characters([' ', '\n', '-', '@', ?a..?z, ?A..?Z, ?0..?9])
+    |> String.replace(~r/@[\w\.]+/, "")
     |> String.split(~r/\W+/, trim: true)
     |> Enum.take(8)
     |> Enum.join("-")
