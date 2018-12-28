b1affed1a3d386181f719f50c3789976d8ce39ba
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-12-28 16:46:00 +0100

    Update.Twitter.from_update/1 extracts in_reply_to_id

--- lib/dolphin/update/twitter.ex
+++ lib/dolphin/update/twitter.ex
@@ -1,6 +1,12 @@
 defmodule Dolphin.Update.Twitter do
-  defstruct [:content]
+  defstruct [:content, :in_reply_to_id]
   alias Dolphin.Update
+
+  def from_update(%Update{text: text, in_reply_to: url}) when is_binary(url) do
+    [_, in_reply_to_id] = Regex.run(~r/https:\/\/twitter.com\/\w+\/status\/(\d+)/, url)
+
+    %Dolphin.Update.Twitter{content: text, in_reply_to_id: in_reply_to_id}
+  end
 
   def from_update(%Update{text: text}) do
     %Dolphin.Update.Twitter{content: text}
--- test/dolphin/update/twitter_test.exs
+++ test/dolphin/update/twitter_test.exs
@@ -8,5 +8,13 @@ defmodule Dolphin.Update.TwitterTest do
                text: "$ man ed\n\n#currentstatus"
              }) == %Dolphin.Update.Twitter{content: "$ man ed\n\n#currentstatus"}
     end
+
+    test "finds the in_reply_to_id for a reply" do
+      assert %Dolphin.Update.Twitter{in_reply_to_id: "1075477062360670208"} =
+               Dolphin.Update.Twitter.from_update(%Dolphin.Update{
+                 text: "@tbdr@twitter.com More convoluted than that, actually. 😅",
+                 in_reply_to: "https://twitter.com/tbdr/status/1075477062360670208"
+               })
+    end
   end
 end
