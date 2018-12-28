7cd75eba3022d6079d9c105ac54f361c300ad2d4
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-12-28 10:40:48 +0100

    UpdateController.create/2 posts updates

--- lib/dolphin_web/controllers/update_controller.ex
+++ lib/dolphin_web/controllers/update_controller.ex
@@ -5,7 +5,8 @@ defmodule DolphinWeb.UpdateController do
     render(conn, "new.html")
   end
 
-  def create(conn, %{"update" => _update_params}) do
-    render(conn, "create.html")
+  def create(conn, %{"update" => update_params}) do
+    {:ok, links} = Dolphin.Update.post(update_params)
+    render(conn, "create.html", links: links)
   end
 end
--- test/dolphin_web/controllers/update_controller_test.exs
+++ test/dolphin_web/controllers/update_controller_test.exs
@@ -9,9 +9,17 @@ defmodule DolphinWeb.UpdateControllerTest do
   end
 
   describe "create update" do
-    test "displays a success message", %{conn: conn} do
-      conn = post(conn, Routes.update_path(conn, :create), update: %{})
-      assert html_response(conn, 200) =~ "Update posted succesfully."
+    test "displays the URLs the update has been posted to", %{conn: conn} do
+      conn =
+        post(conn, Routes.update_path(conn, :create),
+          update: %{text: "$ man ed\n\n#currentstatus"}
+        )
+
+      assert response = html_response(conn, 200)
+      assert response =~ "Update posted succesfully."
+
+      assert response =~
+               "https://github.com/jeffkreeftmeijer/testing/blob/master/2018-12-27-man-ed-currentstatus.md"
     end
   end
 end
