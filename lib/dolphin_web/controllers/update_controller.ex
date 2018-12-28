3f308e708c334c114bdb361d1b33d8c2f8c78553
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-12-28 16:14:30 +0100

    Use Update.from_params/1 from UpdateController.create/2

--- lib/dolphin_web/controllers/update_controller.ex
+++ lib/dolphin_web/controllers/update_controller.ex
@@ -1,12 +1,17 @@
 defmodule DolphinWeb.UpdateController do
   use DolphinWeb, :controller
+  alias Dolphin.Update
 
   def new(conn, _params) do
     render(conn, "new.html")
   end
 
   def create(conn, %{"update" => update_params}) do
-    {:ok, links} = Dolphin.Update.post(update_params)
+    {:ok, links} =
+      update_params
+      |> Update.from_params()
+      |> Update.post()
+
     render(conn, "create.html", links: links)
   end
 end
