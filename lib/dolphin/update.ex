a691605bb22689ea7814e46db4bf1065d33a6d34
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-12-30 12:56:01 +0100

    Update.filename/1 converts slashes and dots to dashes

--- lib/dolphin/update.ex
+++ lib/dolphin/update.ex
@@ -50,11 +50,13 @@ defmodule Dolphin.Update do
       "2018-12-27-man-ed-currentstatus.md"
       iex> Dolphin.Update.filename(%Dolphin.Update{text: "@judofyr@ruby.social because ed is the standard text editor (https://www.gnu.org/fun/jokes/ed-msg.txt)!"})
       "2018-12-27-because-ed-is-the-standard.md"
+      iex> Dolphin.Update.filename(%Dolphin.Update{text: "<https://w3.org/TR/activitypub>\n\n#currentstatus"})
+      "2018-12-27-https-w3-org-tr-activitypub.md"
 
   """
   def filename(%Dolphin.Update{text: text}) do
     (@date.to_iso8601(@date.utc_today) <> "-" <> text)
-    |> Dolphin.Utils.filter_characters([' ', '\n', '-', '@', ?a..?z, ?A..?Z, ?0..?9])
+    |> Dolphin.Utils.filter_characters([' ', '\n', '-', '@', '/', '.', ?a..?z, ?A..?Z, ?0..?9])
     |> String.downcase()
     |> String.replace(~r/@[\w\.]+/, "")
     |> String.split(~r/\W+/, trim: true)
